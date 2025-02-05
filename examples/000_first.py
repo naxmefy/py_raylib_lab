20962b55ad712c0ebd0ede1d53e50c9246dbcddd
MRW Neundorf <ninevillage@gmail.com>
2025-02-05 09:45:42 +0100

    feat(000): resolve first example

--- examples/000_first.py
+++ examples/000_first.py
@@ -1,12 +1,12 @@
 from pyray import *
 from raylib import *
-from os.path import join
 
-init_window(850, 400, "Basics")
+init_window(850, 400, "000 First")
 
 while not window_should_close():
     begin_drawing()
     clear_background(RAYWHITE)
+    draw_text("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
     end_drawing()
 
 close_window()
