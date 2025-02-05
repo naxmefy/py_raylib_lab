b5c54ec34af18359f37818cfe28a24d412e44cc0
MRW Neundorf <ninevillage@gmail.com>
2025-02-05 10:59:36 +0100

    feat(003): add input example from clear code
    GitHub Repository: https://github.com/clear-code-projects/raylib_intro/blob/main/Complete/basics/code/3_input.py
    Youtube Video: https://www.youtube.com/watch?v=UoAsDlUwjy0&t=3188s

---
+++ examples/003_input.py
@@ -0,0 +1,41 @@
+from pyray import *
+from raylib import * 
+from os.path import join
+
+init_window(1920, 1080, "Input")
+set_exit_key(KEY_ESCAPE)
+
+ship_texture = load_texture(join('assets', 'clear-code-raylib-intro', 'basics', 'spaceship.png'))
+ship_pos = Vector2(0,0)
+ship_direction = Vector2(0,0)
+ship_speed = 800
+
+while not window_should_close():
+    # input
+    # mouse input
+    # ship_pos = get_mouse_position()
+    # print(is_mouse_button_down(1)) # state of the mouse button
+    # if is_mouse_button_released(0):
+    #     print('mouse button pressed')
+
+    # keyboard input
+    # print(is_key_down(KEY_A))
+    # if is_key_pressed(KEY_A):
+    #     print('a')
+        
+    # ship input
+    ship_direction.x = int(is_key_down(KEY_RIGHT)) - int(is_key_down(KEY_LEFT))
+    ship_direction.y = int(is_key_down(KEY_DOWN)) - int(is_key_down(KEY_UP))
+    ship_direction = Vector2Normalize(ship_direction)
+    
+    # update 
+    dt = get_frame_time()
+    ship_pos.x += ship_direction.x * ship_speed * dt
+    ship_pos.y += ship_direction.y * ship_speed * dt
+
+    # drawing
+    begin_drawing()
+    clear_background(BLACK)
+    draw_texture_v(ship_texture, ship_pos, WHITE)
+    end_drawing()
+close_window()
