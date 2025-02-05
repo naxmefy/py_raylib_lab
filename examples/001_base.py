651a08a1041c81e802a6415f84601cf9d31a8d56
MRW Neundorf <ninevillage@gmail.com>
2025-02-05 10:49:36 +0100

    feat(001): add base example from clear code
    [GitHub: clear-code-projects/raylib_intro/basics/code/1_base.py](https://github.com/clear-code-projects/raylib_intro/blob/main/Complete/basics/code/1_base.py)
    [Youtube: Basics](https://www.youtube.com/watch?v=UoAsDlUwjy0&t=310s)

---
+++ examples/001_base.py
@@ -0,0 +1,42 @@
+from pyray import *
+from raylib import *
+from os.path import join
+
+init_window(1920,1080, '001 Base')
+
+# import images/textures
+spaceship_texture = load_texture(join('assets', 'clear-code-raylib-intro', 'basics', 'spaceship.png'))
+spaceship_image = load_image(join('assets', 'clear-code-raylib-intro', 'basics', 'spaceship.png'))
+image_color_grayscale(spaceship_image)
+new_texture = load_texture_from_image(spaceship_image)
+
+cowboy_image = load_image(join('assets', 'clear-code-raylib-intro', 'basics','animation', '0.png'))
+image_color_invert(cowboy_image)
+cowboy_texture = load_texture_from_image(cowboy_image)
+
+# import font
+font = load_font(join('assets', 'clear-code-raylib-intro', 'basics', 'Zero Hour.otf'))
+
+while not window_should_close():
+    begin_drawing()
+    clear_background(BLACK)
+    
+    # custom shape drawing
+    draw_pixel(100, 200, RED)
+    draw_pixel_v(Vector2(200,200),WHITE)
+    draw_circle_v(Vector2(1000,600), 200, YELLOW)
+    draw_circle(1000, 600, 100, GREEN)
+    draw_line_ex(Vector2(0,0), Vector2(500,200), 10, (50,0,0,255))
+    
+    # display images
+    draw_texture(spaceship_texture, 0, 0, WHITE)
+    draw_texture_v(new_texture,Vector2(100,0),WHITE)
+    draw_texture(cowboy_texture, 1800, 900, WHITE)
+
+    # displaying text
+    draw_text('Some text',0, 400, 100, WHITE)
+    draw_text_ex(font, 'Some more text', Vector2(0,600), 20, 0, BLUE)
+
+    end_drawing()
+
+close_window()
