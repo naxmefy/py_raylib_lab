bcc5187b44ef35e0f963b6b553799ef628944708
MRW Neundorf <ninevillage@gmail.com>
2025-02-06 10:39:48 +0100

    feat(006): add audio example from clear code
    GitHub Repository: https://github.com/clear-code-projects/raylib_intro/blob/main/Complete/basics/code/6_audio.py
    Youtube Video: https://www.youtube.com/watch?v=UoAsDlUwjy0&t=5337s
    
    Question: why do not unload the sound?

---
+++ examples/006_audio.py
@@ -0,0 +1,21 @@
+from pyray import *
+from raylib import *
+from os.path import join
+
+init_window(1920, 1080, '006 Audio')
+init_audio_device()
+laser_sound = load_sound(join('assets', 'clear-code-raylib-intro', 'basics', 'laser.wav'))
+play_sound(laser_sound)
+
+music = load_music_stream(join('assets', 'clear-code-raylib-intro', 'basics', 'music.wav'))
+play_music_stream(music)
+
+while not window_should_close():
+    update_music_stream(music)
+    begin_drawing()
+    clear_background(BLACK)
+    end_drawing()
+
+unload_music_stream(music)
+close_audio_device()
+close_window()
