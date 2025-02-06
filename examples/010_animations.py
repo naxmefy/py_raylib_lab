17e1fbbbbde862d38a30baa8594a1e9f969181a4
MRW Neundorf <ninevillage@gmail.com>
2025-02-06 12:53:13 +0100

    feat(010): add animations example from clear code
    GitHub Repository: https://github.com/clear-code-projects/raylib_intro/blob/main/Complete/basics/code/10_animations.py
    Youtube Video: https://www.youtube.com/watch?v=UoAsDlUwjy0&t=7874s

---
+++ examples/010_animations.py
@@ -0,0 +1,34 @@
+from pyray import *
+from raylib import *
+from os.path import join
+
+
+class AnimatedSprite:
+    def __init__(self):
+        self.animation_frames = [load_texture(join('assets', 'clear-code-raylib-intro', 'basics', 'animation', f'{i}.png')) for i in range(8)]
+        self.animation_index, self.animation_speed = 0, 5
+
+    def update(self, dt):
+        self.animation_index += self.animation_speed * dt
+
+    def draw(self):
+        draw_texture(self.animation_frames[int(self.animation_index) % len(self.animation_frames)], 0, 0, WHITE)
+
+
+init_window(1920, 1080, '010 Animations')
+# animation_frames = [load_texture(join('assets', 'animation', f'{i}.png')) for i in range(8)]
+# animation_index = 0
+# animation_speed = 5
+animated_sprite = AnimatedSprite()
+
+while not window_should_close():
+    dt = get_frame_time()
+    # animation_index += animation_speed * dt
+    animated_sprite.update(dt)
+    begin_drawing()
+    clear_background(BLACK)
+    animated_sprite.draw()
+    # draw_texture(animation_frames[int(animation_index) % len(animation_frames)],0,0,WHITE)
+    end_drawing()
+
+close_window()
